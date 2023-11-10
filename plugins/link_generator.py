e37e524361c128f7b5ecd0b918021d0e1e806038
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-11 01:37:07 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -6,74 +6,8 @@ from bot import Bot
 from config import ADMINS
 from helper_func import encode, get_message_id
 
-from telethon import types
-
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
-async def choose_batch_command(client: Client, message: Message):
-    # Create an InlineKeyboardMarkup with two buttons
-    keyboard = types.InlineKeyboardMarkup([
-        [
-            types.InlineKeyboardButton("Batch Command 1", callback_data="batch1"),
-            types.InlineKeyboardButton("Batch Command 2", callback_data="batch2"),
-        ]
-    ])
-
-    # Ask the user to choose a batch command
-    await message.reply("Choose a batch command:", reply_markup=keyboard)
-
-
-@Bot.on_callback_query(filters.regex(r'^batch1$'))
-async def batch_command1(client: Client, callback_query: types.CallbackQuery):
-    while True:
-        try:
-            first_message = await client.ask(
-                text="Forward the First Message from DB Channel (with Quotes)..\n\nor Send the DB Channel Post Link",
-                chat_id=message.from_user.id,
-                filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-                timeout=60
-            )
-        except:
-            return
-
-        f_msg_id = await get_message_id(client, first_message)
-
-        if f_msg_id:
-            break
-        else:
-            await first_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
-            continue
-
-    while True:
-        try:
-            second_message = await client.ask(
-                text="Forward the Last Message from DB Channel (with Quotes)..\nor Send the DB Channel Post link",
-                chat_id=message.from_user.id,
-                filters=(filters.forwarded | (filters.text & ~filters.forwarded)),
-                timeout=60
-            )
-        except:
-            return
-
-        s_msg_id = await get_message_id(client, second_message)
-
-        if s_msg_id:
-            break
-        else:
-            await second_message.reply("❌ Error\n\nthis Forwarded Post is not from my DB Channel or this Link is taken from DB Channel", quote=True)
-            continue
-
-    string = f"get-{f_msg_id * abs(client.db_channel.id)}-{s_msg_id * abs(client.db_channel.id)}"
-    base64_string = await encode(string)
-    link = f"https://t.me/{client.username}?start={base64_string}"
-    reply_markup = InlineKeyboardMarkup([[InlineKeyboardButton("🔁 Share URL", url=f'https://telegram.me/share/url?url={link}')]])
-    await second_message.reply_text(f"<b>Here is your link</b>\n\n{link}", quote=True, reply_markup=reply_markup)
-    await callback_query.answer("You chose Batch Command 1")
-
-    # Add the logic for Batch Command 1 here
-
-
-@Bot.on_callback_query(filters.regex(r'^batch2$'))
-async def batch_command2(client: Client, callback_query: types.CallbackQuery):
+async def batch(client: Client, message: Message):
     while True:
         try:
             # Prompt the user to provide the first message from the DB Channel
@@ -128,9 +62,7 @@ async def batch_command2(client: Client, callback_query: types.CallbackQuery):
 
     # Send the generated links to the user
     for link in message_links:
-        await message.reply(f"Here is a link for one of the messages:\n{link}")
-    await callback_query.answer("You chose Batch Command 2")
-
+        await message.reply(f"Here is a link for one of the messages:\n{link}")      
     
 @Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
