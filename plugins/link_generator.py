69ee560a2635121b7aa020cc92bcd6f30801b47e
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-19 00:53:46 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -101,9 +101,21 @@ async def batch(client: Client, message: Message):
             await message.reply_text("Something went wrong..!")
             return
 
+        
         snt_msgs = []
 
         for msg in messages:
+
+            if bool(CUSTOM_CAPTION) & bool(msg.document):
+                caption = CUSTOM_CAPTION.format(previouscaption="" if not msg.caption else msg.caption.html, filename=msg.document.file_name)
+            else:
+                caption = "" if not msg.caption else msg.caption.html
+
+            if DISABLE_CHANNEL_BUTTON:
+                reply_markup = msg.reply_markup
+            else:
+                reply_markup = None
+
             try:
                 snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup,
                                           protect_content=PROTECT_CONTENT)
