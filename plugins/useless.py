a682754add76610089b75f79c0b39f88c748ef13
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-21 04:01:39 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -1,10 +1,12 @@
 from bot import Bot
 from pyrogram.types import Message
 from pyrogram import filters
-from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT, TG_BOT_TOKEN
+from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT, AI
 from datetime import datetime
 from helper_func import get_readable_time
 
+
+AI = is_enabled((environ.get("AI","True")), False)
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
     now = datetime.now()
@@ -13,39 +15,33 @@ async def stats(bot: Bot, message: Message):
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram import Client, filters
 import openai
-from telegram import Update
-from telegram.ext import Filters, MessageHandler, Updater
-
-# Set your OpenAI GPT-3 API key
 openai.api_key = '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'
 
-@Bot.on_message(filters.private & filters.incoming)
-def handle_messages(update: Update, context):
-    # Get the user's message
-    user_message = update.message.text
-
-    # Use the user's message as input to ChatGPT
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=user_message,
-        max_tokens=50  # Adjust as needed
-    )
-
-    # Extract the generated response from ChatGPT
-    chatgpt_reply = response.choices[0].text.strip()
-
-    # Send the generated response back to the user
-    update.message.reply_text(chatgpt_reply)
-
-# Set up the Telegram Bot
-updater = Updater(token='TG_BOT_TOKEN', use_context=True)
-dispatcher = updater.dispatcher
-
-# Register the message handler
-message_handler = MessageHandler(Filters.private & Filters.text & Filters.incoming, handle_messages)
-dispatcher.add_handler(message_handler)
-
-# Start the bot
-updater.start_polling()
-updater.idle()
+@Bot.on_message(filters.private & filters.text)
+async def lazy_answer(client, message):
+    if AI == True: 
+        user_id = message.from_user.id
+        if user_id:
+            try:
+                lazy_users_message = message.text
+                user_id = message.from_user.id
+                response = openai.Completion.create(
+                    model = "text-davinci-003",
+                    prompt = lazy_users_message,
+                    temperature = 0.5, 
+                    max_tokens = 1000,
+                    top_p=1,
+                    frequency_penalty=0.1,
+                    presence_penalty = 0.0,
+                )
+                
+                lazy_response = response.choices[0].text 
+                await message.reply(f"{lazy_response}")
+            except Exception as error:
+                print(error)
+                await message.reply_text(f'Error aa gya Lazy Bhaiya 😀\n\n{error}')
+    else:
+        return
