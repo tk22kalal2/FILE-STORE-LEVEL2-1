489ab2e0c02d1e3a479264488c13d10f2e84b862
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-21 04:52:24 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -18,28 +18,38 @@ from pyrogram import Client, filters
 import openai
 openai.api_key = OPENAI_API
 
+# Import the required library
+from googlesearch import search
+
+# ... (previous code)
+
 @Bot.on_message(filters.private & filters.text)
 async def lazy_answer(client, message):
-    if AI == True: 
+    if AI == True:
         user_id = message.from_user.id
-        if user_id:
-            try:
-                lazy_users_message = message.text
-                user_id = message.from_user.id
-                prompt = f"Miss Dopamine, a female doctor, says: {lazy_users_message}"
-                response = openai.Completion.create(
-                    model = "text-davinci-003",
-                    prompt = lazy_users_message,
-                    temperature = 0.5, 
-                    max_tokens = 1000,
-                    top_p=1,
-                    frequency_penalty=0.1,
-                    presence_penalty = 0.0,
-                )                
-                lazy_response = response.choices[0].text 
-                await message.reply(f"{lazy_response}")
-            except Exception as error:
-                print(error)
-                await message.reply_text(f'Error aa gya Lazy Bhaiya 😀\n\n{error}')
+        try:
+            lazy_users_message = message.text
+            prompt = f"Miss Dopamine, a female doctor, says: {lazy_users_message}"
+            response = openai.Completion.create(
+                model="text-davinci-003",
+                prompt=lazy_users_message,
+                temperature=0.5,
+                max_tokens=1000,
+                top_p=1,
+                frequency_penalty=0.1,
+                presence_penalty=0.0,
+            )
+            lazy_response = response.choices[0].text
+
+            # Perform a Google image search
+            image_query = f"{lazy_response} image"
+            for url in search(image_query, stop=1):
+                # Reply with the generated response and the first image URL from Google search
+                await message.reply_photo(url, caption=lazy_response)
+
+        except Exception as error:
+            print(error)
+            await message.reply_text(f'Error aa gya Lazy Bhaiya 😀\n\n{error}')
     else:
         return
+
