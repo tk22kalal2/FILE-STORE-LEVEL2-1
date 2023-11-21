b6248147b1980fbb0276848d3edf4a0c0500f279
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-21 06:31:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -37,7 +37,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello Dr.{first}\n\nUSE ME AS OPENAI. ASK ME QUESTIONS RELATED TO STUDY.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello Dr.{first}\n\nUSE ME AS OPENAI. ASK ME QUESTIONS RELATED TO STUDY.\n\n@pavoladder2_bot")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
