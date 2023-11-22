d8e939e426c10598692ed463816257230da227a4
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-11-22 09:20:26 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -5,10 +5,17 @@ from pyrogram.enums import ParseMode
 from config import ADMINS, BOT_STATS_TEXT, USER_REPLY_TEXT, AI, OPENAI_API, AI_LOGS
 from datetime import datetime
 from helper_func import get_readable_time
-from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup
 from pyrogram import Client, filters
 import openai
 openai.api_key = OPENAI_API
+
+custom_keyboard = ReplyKeyboardMarkup(
+    [
+        ["newchat⚡️"],
+    ],
+    resize_keyboard=True
+)
 
 @Bot.on_message(filters.command('stats') & filters.user(ADMINS))
 async def stats(bot: Bot, message: Message):
