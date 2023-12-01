825665d33bc1d557ab200c1e954eafea85ef6858
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-12-02 00:43:40 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -102,7 +102,7 @@ async def start_command(client: Client, message: Message):
     else:        
         reply_markup = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("🔗 Visit Pavoladder", url="https://t.me/FileXtera2_bot?start=Z2V0LTcwNDQyMTE4MjM5Nzg0OQ")],
+                [InlineKeyboardButton("🔗 Visit Pavoladder", url="https://t.me/FileXtera2_bot?start=Z2V0LTczNTQ4Mzg1MTg5MTkyMg")],
                 [InlineKeyboardButton("😊 About Me", callback_data="about"), InlineKeyboardButton("🔒 Close", callback_data="close")],
             ]
         )
