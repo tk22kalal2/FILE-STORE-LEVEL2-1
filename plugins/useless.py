fc7353312489b2ca8c75dae62e8be15ae59ca9e5
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-12-05 23:32:01 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -30,7 +30,7 @@ async def stats(bot: Bot, message: Message):
 UNSPLASH_API_KEY = "7vqvT8gDlgCu18v-GuT_EPyqxtRYPMVGNBmpBE59jac"
 
 user_conversations = {}
-
+lazy_users_message = None
 @Client.on_message((filters.private & filters.text) | (filters.command("newchat") | filters.regex('newchat⚡️')))
 async def lazy_answer(client: Client, message: Message):
     if AI:
