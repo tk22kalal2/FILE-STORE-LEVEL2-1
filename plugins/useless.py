2f570cfa6c517875d0731171065b679269801314
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2023-12-28 05:39:21 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -9,8 +9,9 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyb
 from pyrogram import Client, filters
 import openai
 import requests
+import google.generativeai as genai
 
-openai.api_key = OPENAI_API
+genai.configure(api_key="OPENAI_API")
 
 buttonz = ReplyKeyboardMarkup(
     [
@@ -26,23 +27,6 @@ async def stats(bot: Bot, message: Message):
     time = get_readable_time(delta.seconds)
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
-def fetch_unsplash_image(query):
-    # Use the Unsplash API to fetch an image based on the query    
-    try:
-        response = requests.get(
-            f"https://api.unsplash.com/photos/random?query={query}",
-            headers={"Authorization": f"Client-ID {UNSPLASH_API_KEY}"}
-        )
-        data = response.json()
-        image_url = data.get("urls", {}).get("regular")
-        return image_url
-    except Exception as e:
-        print(f"Error fetching Unsplash image: {e}")
-        return None
-        
-# Add your Unsplash API key here
-UNSPLASH_API_KEY = "7vqvT8gDlgCu18v-GuT_EPyqxtRYPMVGNBmpBE59jac"
-
 user_conversations = {}
 
 @Client.on_message((filters.private & filters.text) | (filters.command("newchat") | filters.regex('newchat⚡️')))
@@ -57,29 +41,6 @@ async def lazy_answer(client: Client, message: Message):
                     response_text = "New chat started. Ask me anything!"
                     await message.reply(response_text)
                     return
-                           
-                # Handle specific user messages
-                lazy_users_message = message.text.lower().strip()
-                if lazy_users_message in ["hi", "hello"]:
-                    response_text = "Hi Doctor! I am ChatGPT . Ask me anything related to medical study Questions."
-                    await message.reply(response_text)
-                    return
-
-                if message.text.lower().strip() == "#diagram":
-                    # Get the user's previous messages
-                    user_messages = user_conversations.get(user_id, [])
-                    # Use the user's messages as a prompt
-                    prompt = "\n".join(user_messages)
-                    # Fetch a relevant image from Unsplash.com based on the user's messages
-                    image_url = fetch_unsplash_image(prompt)
-    
-                    if image_url:
-                    # Send the image to the user
-                        await client.send_photo(message.chat.id, photo=image_url, caption="Here's a relevant diagram:")
-                    else:
-                        await message.reply("Sorry, I couldn't find a relevant diagram image.")
-                    return
-
 
                 # Get the user's previous messages
                 user_messages = user_conversations.get(user_id, [])
@@ -88,18 +49,43 @@ async def lazy_answer(client: Client, message: Message):
                 # Use the user's messages as a prompt
                 prompt = "\n".join(user_messages)
 
-                response = openai.Completion.create(
-                    model="text-davinci-003",
-                    prompt=prompt,
-                    temperature=0.5,
-                    max_tokens=1000,
-                    top_p=1,
-                    frequency_penalty=0.1,
-                    presence_penalty=0.0,
-                )
+                # Set up the model
+                generation_config = {
+                    "temperature": 0.9,
+                    "top_p": 1,
+                    "top_k": 1,
+                    "max_output_tokens": 2048,
+                }
+
+                safety_settings = [
+                    {
+                        "category": "HARM_CATEGORY_HARASSMENT",
+                        "threshold": "BLOCK_ONLY_HIGH"
+                    },
+                    {
+                        "category": "HARM_CATEGORY_HATE_SPEECH",
+                        "threshold": "BLOCK_ONLY_HIGH"
+                    },
+                    {
+                        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
+                        "threshold": "BLOCK_ONLY_HIGH"
+                    },
+                    {
+                        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
+                        "threshold": "BLOCK_ONLY_HIGH"
+                    },
+                ]
+
+                model = genai.GenerativeModel(model_name="gemini-pro",
+                                              generation_config=generation_config,
+                                              safety_settings=safety_settings)
+                prompt_parts = [prompt]
+
+                response = model.generate_content(prompt_parts)
+
                 footer_credit = "<b>ADMIN ID:</b>-@talktomembbs_bot\n<b>MBBS LECTURES:-</b><a href='https://sites.google.com/view/pavoladdder'>CLICK HERE</a>"
 
-                lazy_response = response.choices[0].text
+                lazy_response = response.text
 
                 await client.send_message(AI_LOGS, text=f"<b>Name - {message.from_user.mention}\n{user_id}\n</b>CONVERSATION HISTORY:-\n{prompt}\n</b>ANSWER:-\n{lazy_response}", parse_mode=ParseMode.HTML)
 
@@ -112,4 +98,3 @@ async def lazy_answer(client: Client, message: Message):
                 print(error)
     else:
         return
-
