40920a3de28ddd5a784271b0b92343c25388b274
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-01-05 00:54:53 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -60,28 +60,9 @@ async def lazy_answer(client: Client, message: Message):
                     "temperature": 1,
                     "top_p": 1,
                     "top_k": 1,
-                    "max_output_tokens": 1500,
+                    "max_output_tokens": 1000,
                 }
-
-                safety_settings = [
-                    {
-                        "category": "HARM_CATEGORY_HARASSMENT",
-                        "threshold": "BLOCK_ONLY_HIGH"
-                    },
-                    {
-                        "category": "HARM_CATEGORY_HATE_SPEECH",
-                        "threshold": "BLOCK_ONLY_HIGH"
-                    },
-                    {
-                        "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-                        "threshold": "BLOCK_ONLY_HIGH"
-                    },
-                    {
-                        "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-                        "threshold": "BLOCK_ONLY_HIGH"
-                    },
-                ]
-
+                
                 model = genai.GenerativeModel(model_name="gemini-pro",
                                               generation_config=generation_config,
                                               safety_settings=safety_settings)
