f981a605dbedede303faa17202cff5fcbce27a6e
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-01-05 00:57:27 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -65,7 +65,7 @@ async def lazy_answer(client: Client, message: Message):
                 
                 model = genai.GenerativeModel(model_name="gemini-pro",
                                               generation_config=generation_config,
-                                              safety_settings=safety_settings)
+                                              )
                 prompt_parts = [prompt]
 
                 response = model.generate_content(prompt_parts)
