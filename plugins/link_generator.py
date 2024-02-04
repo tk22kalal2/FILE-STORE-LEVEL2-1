e67ecc32c8cf4980e548242a3a690ecb70a1fe80
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-02-04 21:59:59 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -122,7 +122,7 @@ async def batch(client: Client, message: Message):
                 snt_msg = await msg.copy(chat_id=message.from_user.id, caption=caption, parse_mode=ParseMode.HTML, reply_markup=reply_markup,
                               protect_content=PROTECT_CONTENT)
                 try:
-                    await snt_msg.forward(chat_id='FCHANNEL_ID')
+                    await snt_msg.forward(chat_id=FCHANNEL_ID)
                     await asyncio.sleep(1)
                     snt_msgs.append(snt_msg)
                 except FloodWait as e:
