17310e3d83ee5cbf7369b11cf669eec399f21208
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-02-10 18:15:26 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -19,12 +19,6 @@ SECONDS = int(os.getenv("SECONDS", "10")) #add time im seconds for waitingwaitin
 
 @Bot.on_message(filters.command('start') & filters.private & subscribed)
 async def start_command(client: Client, message: Message):
-    id = message.from_user.id
-    if not await present_user(id):
-        try:
-            await add_user(id)
-        except:
-            pass
     text = message.text
         
     if len(text) > 7:
@@ -179,57 +173,67 @@ WAIT_MSG = "Please wait..."
 REPLY_ERROR = "Please reply to a message to broadcast."
 
 
-@Bot.on_message(filters.command('users') & filters.private & filters.user(ADMINS))
-async def get_users(client: Bot, message: Message):
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
+from config import ADMINS, CHANNEL_ID
+from database.database import add_user, del_user, full_userbase, present_user
+
+async def get_users(client: Client, message: Message):
     try:
         chat_info = await client.get_chat(chat_id=CHANNEL_ID)
         member_count = chat_info.members_count
+        id = message.from_user.id
+        if not await present_user(id):
+            try:
+                await add_user(id, CHANNEL_ID)
+            except:
+                pass
         await message.reply(f"Total {member_count} users are in the channel.")
     except Exception as e:
         await message.reply(f"Error: {str(e)}")
 
-@Bot.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
-async def send_text(client: Bot, message: Message):
+@Client.on_message(filters.private & filters.command('broadcast') & filters.user(ADMINS))
+async def send_text(client: Client, message: Message):
     if message.reply_to_message:
-        try:
-            chat_info = await client.get_chat(chat_id=CHANNEL_ID)
-            member_count = chat_info.members_count
-            pls_wait = await message.reply(f"<i>Broadcasting Message to {member_count} users.. This will Take Some Time</i>")
-            broadcast_msg = message.reply_to_message
-            total = 0
-            successful = 0
-            blocked = 0
-            deleted = 0
-            unsuccessful = 0
-            
-            async for member in client.iter_chat_members(chat_id=CHANNEL_ID):
-                try:
-                    await broadcast_msg.copy(member.user.id)
-                    successful += 1
-                except FloodWait as e:
-                    await asyncio.sleep(e.x)
-                    await broadcast_msg.copy(member.user.id)
-                    successful += 1
-                except UserIsBlocked:
-                    blocked += 1
-                except InputUserDeactivated:
-                    deleted += 1
-                except:
-                    unsuccessful += 1
-                    pass
-                total += 1
-            
-            status = f"""<b><u>Broadcast Completed</u>
+        query = await full_userbase()
+        broadcast_msg = message.reply_to_message
+        total = 0
+        successful = 0
+        blocked = 0
+        deleted = 0
+        unsuccessful = 0
+        
+        pls_wait = await message.reply("<i>Broadcasting Message.. This will Take Some Time</i>")
+        for chat_id in query:
+            try:
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+                await broadcast_msg.copy(chat_id)
+                successful += 1
+            except UserIsBlocked:
+                await del_user(chat_id)
+                blocked += 1
+            except InputUserDeactivated:
+                await del_user(chat_id)
+                deleted += 1
+            except:
+                unsuccessful += 1
+                pass
+            total += 1
+        
+        status = f"""<b><u>Broadcast Completed</u>
 
 Total Users: <code>{total}</code>
 Successful: <code>{successful}</code>
 Blocked Users: <code>{blocked}</code>
 Deleted Accounts: <code>{deleted}</code>
 Unsuccessful: <code>{unsuccessful}</code></b>"""
-            
-            await pls_wait.edit(status)
-        except Exception as e:
-            await message.reply(f"Error: {str(e)}")
+        
+        return await pls_wait.edit(status)
 
     else:
         msg = await message.reply(REPLY_ERROR)
