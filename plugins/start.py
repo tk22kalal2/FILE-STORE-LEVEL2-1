5d9e5777fc946c6aceb8f5bd703e1428411e1a0c
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-02-10 20:21:21 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
 from bot import Bot
 from config import ADMINS, FORCE_MSG, START_MSG, CUSTOM_CAPTION, DISABLE_CHANNEL_BUTTON, PROTECT_CONTENT, CHANNEL_ID
-from helper_func import subscribed, encode, decode, get_messages
+from helper_func import subscribed, encode, decode, get_messages, get_message_id
 from database.database import add_user, del_user, full_userbase, present_user
 
 SECONDS = int(os.getenv("SECONDS", "10")) #add time in seconds for waiting before delete
