272d8778451d742403def8cb96850c9e0f336da8
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-19 01:49:48 +0530

    Update useless.py

--- plugins/useless.py
+++ plugins/useless.py
@@ -13,7 +13,4 @@ async def stats(bot: StreamBot, message: Message):
     await message.reply(BOT_STATS_TEXT.format(uptime=time))
 
 
-@StreamBot.on_message(filters.private & filters.incoming)
-async def useless(_,message: Message):
-    if USER_REPLY_TEXT:
-        await message.reply(USER_REPLY_TEXT)
+
