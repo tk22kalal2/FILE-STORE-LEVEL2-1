3b1b8b51cba71149446e20a84e5a692c0e3a8274
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-18 22:55:25 +0530

    Update __init__.py

--- Adarsh/bot/__init__.py
+++ Adarsh/bot/__init__.py
@@ -9,7 +9,7 @@ StreamBot = Client(
     name='Web Streamer',
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
+    bot_token=TG_BOT_TOKEN,
     sleep_threshold=Var.SLEEP_THRESHOLD,
     workers=Var.WORKERS
 )
