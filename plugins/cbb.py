8f8ba3864b3024be6c2e7f756db87a2dfbdc5eac
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-19 01:38:14 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -1,7 +1,7 @@
 #(©)Codexbotz
 
 from pyrogram import __version__
-from bot import Bot
+from Adarsh.bot import StreamBot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
