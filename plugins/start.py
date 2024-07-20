b3aa7c5b96656ea01e330a80e9d27cbdd08c5fdb
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2024-07-20 14:51:22 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -74,13 +74,17 @@ async def start_command(client: Client, message: Message):
                 ("" if not msg.caption else msg.caption.html)
             )
 
-            reply_markup = None if DISABLE_CHANNEL_BUTTON else msg.reply_markup
+            if DISABLE_CHANNEL_BUTTON:
+                reply_markup = msg.reply_markup
+            else:
+                reply_markup = None
 
             try:
                 snt_msg = await msg.copy(
                     chat_id=message.from_user.id,
                     caption=caption,
                     parse_mode=ParseMode.HTML,
+                    reply_markup=reply_markup,
                     protect_content=PROTECT_CONTENT
                 )
 
