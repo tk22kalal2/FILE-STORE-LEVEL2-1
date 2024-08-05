9947f0ff72e00b8e6f3670c140446d9018e1ab4f
hello world <159936375+tk22kalal2@users.noreply.github.com>
2024-08-06 00:54:58 +0530

    Update link_generator.py

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -2,7 +2,7 @@
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from bot import Bot
+from Adarsh.bot import StreamBot
 from config import ADMINS, CUSTOM_CAPTION, CHANNEL_ID
 from helper_func import encode, get_message_id
 from pyrogram.errors import FloodWait, UserIsBlocked, InputUserDeactivated
@@ -16,7 +16,7 @@ def clean_caption(caption):
     caption = re.sub(r'http\S+', '', caption)
     return caption
     
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
+@StreamBot.on_message(filters.private & filters.user(ADMINS) & filters.command('batch'))
 async def batch(client: Client, message: Message):
     while True:
         try:
@@ -107,7 +107,7 @@ async def batch(client: Client, message: Message):
     await message.reply("Batch processing completed.")
     
     
-@Bot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
+@StreamBot.on_message(filters.private & filters.user(ADMINS) & filters.command('genlink'))
 async def link_generator(client: Client, message: Message):
     while True:
         try:
